f92fa0e8b6f42a30519fdbb4d85458c70b361cc8
Rushikesh <Rushi@example.com>
2023-08-16 13:45:20 +0530

    All Set

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+
+let a = document.getElementById("btnresume");
+a.addEventListener("click", function(){
+        window.open("https://www.google.com", "_blank")
+        // win.focus();
+    })
+
+
+function onclickmenu(){
+    document.getElementById("ham-hide").classList.toggle("change")
+}
