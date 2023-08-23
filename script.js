ef00172054b597de2fcc56258654fb9114fa1502
Rushikesh <Rushi@example.com>
2023-08-23 21:02:56 +0530

    add various links

--- script.js
+++ script.js
@@ -11,3 +11,13 @@ a.addEventListener("click", function(){
 function onclickmenu(){
     document.getElementById("ham-hide").classList.toggle("change")
 }
+
+let blogX_sc = document.getElementById("bx-sc");
+blogX_sc.addEventListener("click", function(){
+        window.open("https://github.com/RushikeshChouthmahal/Portfolio.github.io", "_blank")
+    })
+
+let blogX_wl = document.getElementById("bx-wl");
+blogX_wl.addEventListener("click", function(){
+        window.open("https://rushikeshchouthmahal.github.io/blogX.github.io/", "_blank")
+    })
